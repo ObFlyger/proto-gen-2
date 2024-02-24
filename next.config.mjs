80ff958855c7269a5e52b20ed986c8cf0591af0c
ObFlyger <65856223+ObFlyger@users.noreply.github.com>
2024-02-24 21:35:14 +0100

    wip: fixed cors 2

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,24 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   async headers() {
     return [
       {
         source: "/:path*",
         headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
           {
             key: "Access-Control-Allow-Origin",
-            value: "*",
+            value: "https://proto-gen-2.vercel.app",
           },
           {
             key: "Access-Control-Allow-Methods",
-            value: "GET",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
           },
           {
             key: "Access-Control-Allow-Headers",
-            value: "Origin, X-Requested-With, Content-Type, Accept",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
           },
         ],
       },
